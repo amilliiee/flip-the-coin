2be9c3a14af620114fdbc5b6af5e877d77bc09c3
amelialsylvest <amelialsylvest@gmail.com>
2025-04-22 13:51:40 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById('flip-btn');
+const coinBtn = document.getElementById('coin-btn');
+const result = document.getElementById('result');
+const coin = document.getElementById('coin');
+
+btn.addEventListener("click", () => {
+  const rslt = Math.random() < 0.5 ? "Heads" : "Tails";
+
+  coin.src = rslt === "Heads" ? "./resources/heads.svg" : "./resources/tails.svg";
+  result.textContent = rslt;
+});
+
+coinBtn.addEventListener("click", () => {
+  const rslt = Math.random() < 0.5 ? "Heads" : "Tails";
+
+  coin.src = rslt === "Heads" ? "./resources/heads.svg" : "./resources/tails.svg";
+  result.textContent = rslt;
+});
