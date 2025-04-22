3e1d65aa336e3bf419728405a3077831be3f6a02
amelialsylvest <amelialsylvest@gmail.com>
2025-04-22 14:04:10 +0000

    update font weights

--- main.js
+++ main.js
@@ -14,5 +14,6 @@ coinBtn.addEventListener("click", () => {
   const rslt = Math.random() < 0.5 ? "Heads" : "Tails";
 
   coin.src = rslt === "Heads" ? "./resources/heads.svg" : "./resources/tails.svg";
+  coin.alt = rslt === "Heads" ? "Coin head" : "Coin tail";
   result.textContent = rslt;
 });
